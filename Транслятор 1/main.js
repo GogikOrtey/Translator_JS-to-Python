1938ebee08fe6a11f07ed2d2efa86a9deabdcebc
Gogik <gog.ortey@yandex.ru>
2024-01-26 00:37:21 +1000

    2 - Добавил чтение входного файла, и процедуру очистки массива, куда он загружается

--- Транслятор 1/main.js
+++ Транслятор 1/main.js
@@ -9,11 +9,49 @@ function print(value) {
     Входной код находится в файле Input.txt, в этой директории
 */
 
+/// ---------- LEXER ---------- ///
+
+let inputMass; // Массив, который будет хранить все строки входного файла
+
 // Читаем входной файл
 fs.readFile('Транслятор 1/Input.txt', 'utf8', function(err, data) {
+    // Помним, что чтение файла - операция асинхронная
     if (err) {
         console.error("Не удалось открыть файл: ", err);
+        process.exit(1); // Завершаем программу, если ошибка
     } else {
-        console.log(data);
+        //console.log(data);
+        inputMass = data.split('\n');        
+
+        LexerClearing(inputMass);
     }
 });
+
+// Очищает входной файл от пробелов, табов, и других спецсимволов
+function LexerClearing(inputMass) {
+    print(inputMass)
+
+    // Удаляем пустые строки
+    inputMass = inputMass.filter(line => line.trim() !== '');
+
+    // Обрабатываем каждую строку
+    inputMass = inputMass.map(line => {
+        // Удаляем символы конца строки
+        line = line.replace(/\r/g, '');
+
+        // Заменяем все табы на 1 пробел
+        line = line.replace(/\t/g, ' ');
+
+        // Оставляем только один пробел, если встречено больше 1 пробела подряд
+        line = line.replace(/ +/g, ' ');
+
+        // Удаляем пробелы в начале и в конце строки
+        line = line.trim();
+
+        return line;
+    });
+
+    print(inputMass)
+
+    return inputMass;
+}
